3ab70cfc2b22c803a92ce10ee06fefe7c3722549
asepsetiawan9 <zep.setiawan09@gmail.com>
2022-10-12 17:02:59 +0700

    release app and change icon app

--- android/app/src/main/java/com/fazztrack/arrtos/MainActivity.java
+++ android/app/src/main/java/com/fazztrack/arrtos/MainActivity.java
@@ -4,6 +4,9 @@ import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
 import org.devio.rn.splashscreen.SplashScreen;
+//ini new
+// import android.os.Bundle;
+//
 
 public class MainActivity extends ReactActivity {
 
@@ -11,6 +14,12 @@ public class MainActivity extends ReactActivity {
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
+   //new
+  //  @Override
+  //  protected void onCreate(Bundle savedInstanceState){
+  //   super.onCreate(null);
+  //  }
+   //ini
   @Override
   protected void onCreate(Bundle savedInstanceState) {
       SplashScreen.show(this);  // here
